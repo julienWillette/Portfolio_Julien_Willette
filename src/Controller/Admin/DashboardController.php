72e429ac6431bccd40f16fbe3481a87ef3665de6
julien willette <julien.willette@gmail.com>
2021-01-23 18:31:48 +0100

    add auth

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,7 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+/**
+ * @IsGranted("ROLE_ADMIN")
+ */
 
 class DashboardController extends AbstractDashboardController
 {
