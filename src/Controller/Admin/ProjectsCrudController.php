d2e80c2bf884a89a2c4848c1fd3a8723be8e8309
julien willette <julien.willette@gmail.com>
2021-03-24 11:58:13 +0100

    add delete for admin

--- src/Controller/Admin/ProjectsCrudController.php
+++ src/Controller/Admin/ProjectsCrudController.php
@@ -3,7 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Projects;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
@@ -26,5 +30,28 @@ class ProjectsCrudController extends AbstractCrudController
             TextField::new('url_github'),
             AssociationField::new('techno'),
         ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $deleteAction = Action::new('Delete', '')
+        ->setIcon('fas fa-trash')
+        ->linkToCrudAction('deleteAction');
+
+        return $actions
+        ->add(Crud::PAGE_INDEX, Action::DETAIL)
+        ->add(Crud::PAGE_INDEX, $deleteAction)
+        ->remove(Crud::PAGE_INDEX, Action::DELETE)
+        ->remove(Crud::PAGE_DETAIL, Action::DELETE);
+    }
+    public function deleteAction(AdminContext $context)
+    {
+        $id = $context->getRequest()->query->get('entityId');
+        $entity = $this->getDoctrine()->getRepository(Projects::class)->find($id);
+
+        $this->deleteEntity($this->get('doctrine')->getManagerForClass($context->getEntity()->getFqcn()), $entity);
+        $this->addFlash('success', 'Article supprimé');
+        // ici modifier la redirection selon ou l'admin doit être redirigé après l'action delete
+        return $this->redirect('admin');
     }
 }
